a20c912a6c36efd0587e87eb8e4434e4e17c342d
Bizuayehu Nigatu <NIGBIZ0122@den.coloradotech.edu>
2018-10-22 17:25:48 -0600

    Aggregates are added

--- Section-04/Lecture4.sql
+++ Section-04/Lecture4.sql
@@ -66,7 +66,59 @@ GROUP BY Country,[NAME]
 ORDER BY Country ASC, Language_Percentage DESC;
 
 /*==============================================================================================
- * 4) Using basic HAVING clause 
+ * 4) Legal aggregates
+ *==============================================================================================*/
+
+-- Only aggregate column without group by
+SELECT MAX(Elevation)
+FROM dbo.Mountain;
+
+-- None aggregates column with a group by clause
+SELECT Mountains,MAX(Elevation)
+FROM dbo.Mountain
+GROUP BY Mountains;
+
+-- Multiple aggregate columns 
+SELECT MAX(Elevation),MIN(Elevation),AVG(Elevation),SUM(Elevation),COUNT(Elevation)
+FROM dbo.Mountain;
+
+/*==============================================================================================
+ * 5) Illegal aggregates
+ *==============================================================================================*/
+-- Aggregates in a Where clause
+SELECT *
+FROM dbo.Mountain
+WHERE Elevation = MAX(Elevation);
+
+-- None aggregates column without a group by clause
+SELECT Mountains,MAX(Elevation)
+FROM dbo.Mountain;
+
+-- Neste aggregate columns 
+SELECT MAX(AVG(Elevation))
+FROM dbo.Mountain;
+
+
+/*==============================================================================================
+ * 6) Statistics in SQL
+ *==============================================================================================*/
+SELECT COUNT(*) AS Count_Every_Column,
+   COUNT(Mountains) AS Count_Mountains,
+   COUNT(DISTINCT Mountains) AS Count_Distinct_Mountains
+FROM dbo.Mountain;
+
+
+-- Distinct another form
+SELECT Mountains
+FROM dbo.Mountain
+GROUP BY Mountains;
+
+SELECT DISTINCT Mountains
+FROM dbo.Mountain;
+
+
+/*==============================================================================================
+ * 7) Using basic HAVING clause 
  *==============================================================================================*/
 
 -- Unlike ORDER BY clause we cannot use WHERE clause to filter aggregate functions
